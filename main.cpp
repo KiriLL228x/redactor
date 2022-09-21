ddc4a6125defcc2e83b05472cbb4a830ee74b798
KiriLL228x <100783309+KiriLL228x@users.noreply.github.com>
2022-09-21 19:31:43 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 #include "TXLib.h"
-
-void drawButton(int x, int y, const char* text)
+//структура кнопки
+struct Button
+{
+  int x;
+  int y;
+  const char* text;
+};
+//функция рисования кнопки
+void drawButton(Button btn)
 {
     //x=30 y=30
         txSetColor(TX_BLACK);
         txSetFillColor(TX_WHITE);
-        Win32::RoundRect (txDC(), x, y, x+150, y+40, 30, 30);
+        Win32::RoundRect (txDC(), btn.x, btn.y, btn.x+150, btn.y+40, 30, 30);
         txSetFillColor(TX_BLACK);
 
         txSetColor(TX_BLACK);
         txSelectFont("Times  New Roman", 32);
-        txDrawText(x, y, x+150,y+40, text);
+        txDrawText(btn.x, btn.y, btn.x+150,btn.y+40, btn.text);
 }
 
 int main()
@@ -33,18 +40,30 @@ int main()
         txSetColor(TX_WHITE);
         txClear();
          txBitBlt(txDC(), 0, 0, 1200, 900, Fon2);
+    //Массив
+        Button btn[10];
+        btn[0] = {30,30,"Части стен"};
+        btn[1] = {200,30,"Крыша"};
+        btn[2] = {370,30,"Земли"};
+        //Рисование кнопок
+        for(int nk=0; nk<5; nk++)
+        {
+        drawButton(btn[nk]);
 
-        //Кнопка 1
-         drawButton(30,30,"Части стен");
-        //Кнопка 2
-         drawButton(200,30,"Крыша");
-        //Кнопка 3
-         drawButton(370,30,"Земли");
+        }
 
         txTransparentBlt(txDC(), 1000, 400, 90, 90, block1, 0, 0, TX_WHITE);
         txTransparentBlt(txDC(), 1000, 300, 90, 90, block2, 0, 0, TX_WHITE);
         txTransparentBlt(txDC(), 1000, 200, 90, 90, block3, 0, 0, TX_WHITE);
-
+     //Клик на кнопку 1
+        if(txMouseButtons() == 1 &&
+           txMouseX()>=btn[1].x &&
+           txMouseX()<=btn[1].x+170&&
+           txMouseY()>=btn[1].y &&
+           txMouseY()<=btn[1].y+70)
+         {
+             txTransparentBlt(txDC(), 1000, 100, 90, 90, block1, 0, 0, TX_WHITE);
+         }
 
         txSleep(50);
         txEnd();
