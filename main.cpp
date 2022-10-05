91725fa527ebf5f0a995dac57d7cf63ef8b6b2a8
KiriLL228x <100783309+KiriLL228x@users.noreply.github.com>
2022-10-05 19:33:01 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ struct Pictures
     HDC image;
     int w;
     int h;
+    bool visible;
 };
 //структура кнопки
 struct Button
@@ -40,8 +41,13 @@ bool click(Button btn)
 
 void drawPicture(Pictures pct)
 {
-  txTransparentBlt(txDC(), pct.x, pct.y, pct.w, pct.h, pct.image, 0, 0, TX_WHITE);
+if(pct.visible)
+{
+    txTransparentBlt(txDC(), pct.x, pct.y, pct.w, pct.h, pct.image, 0, 0, TX_WHITE);
+}
+
 };
+
 int main()
 {
     txCreateWindow (1200, 800);
@@ -57,21 +63,16 @@ int main()
         btn[2] = {370,30,"Земли"};
 
         Pictures menuPic[10];
-        menuPic[0] = {1000, 400, txLoadImage("img/Imgmenu/Wall1.bmp"), 90,90};
-        menuPic[1] = {1000, 300, txLoadImage("img/Imgmenu/Wall2.bmp"), 90,90};
-        menuPic[2] = {1000, 200, txLoadImage("img/Imgmenu/Wall3.bmp"), 90,90};
-        menuPic[3] = {1000, 400, txLoadImage("img/Imgmenu/Ground_10.bmp"), 90,90};
-        menuPic[4] = {1000, 300, txLoadImage("img/Imgmenu/Ground_11.bmp"), 90,90};
-        menuPic[5] = {1000, 200, txLoadImage("img/Imgmenu/Ground_12.bmp"), 90,90};
-        menuPic[6] = {1000, 400, txLoadImage("img/Imgmenu/Roof_A_04.bmp"), 90,40};
-        menuPic[7] = {1000, 300, txLoadImage("img/Imgmenu/Roof_A_05.bmp"), 90,40};
-        menuPic[8] = {1000, 200, txLoadImage("img/Imgmenu/Roof_B_04.bmp"), 90,40};
-        menuPic[9] = {1000, 100, txLoadImage("img/Imgmenu/Roof_B_05.bmp"), 90,40};
-
-
-    bool blockVisible = false;
-    bool earthVisible = false;
-    bool roofVisible = false;
+        menuPic[0] = {1000, 400, txLoadImage("img/Imgmenu/Wall1.bmp"), 90,90, false};
+        menuPic[1] = {1000, 300, txLoadImage("img/Imgmenu/Wall2.bmp"), 90,90, false};
+        menuPic[2] = {1000, 200, txLoadImage("img/Imgmenu/Wall3.bmp"), 90,90, false};
+        menuPic[3] = {1000, 400, txLoadImage("img/Imgmenu/Ground_10.bmp"), 90,90, false};
+        menuPic[4] = {1000, 300, txLoadImage("img/Imgmenu/Ground_11.bmp"), 90,90, false};
+        menuPic[5] = {1000, 200, txLoadImage("img/Imgmenu/Ground_12.bmp"), 90,90, false};
+        menuPic[6] = {1000, 400, txLoadImage("img/Imgmenu/Roof_A_04.bmp"), 90,40, false};
+        menuPic[7] = {1000, 300, txLoadImage("img/Imgmenu/Roof_A_05.bmp"), 90,40, false};
+        menuPic[8] = {1000, 200, txLoadImage("img/Imgmenu/Roof_B_04.bmp"), 90,40, false};
+        menuPic[9] = {1000, 100, txLoadImage("img/Imgmenu/Roof_B_05.bmp"), 90,40, false};
 
 
     while(!GetAsyncKeyState(VK_ESCAPE))
@@ -86,52 +87,32 @@ int main()
         {
             drawButton(btn[nk]);
         }
-
+//видимость картинок меню
         if(click(btn[0]))
         {
-            blockVisible = true;
-            earthVisible = false;
-            roofVisible = false;
+            menuPic[0].visible=true;
+            menuPic[1].visible=true;
+            menuPic[2].visible=true;
         }
         if(click(btn[1]))
         {
-            blockVisible = false;
-            earthVisible = false;
-            roofVisible = true;
+            menuPic[3].visible=true;
+            menuPic[4].visible=true;
+            menuPic[5].visible=true;
         }
         if(click(btn[2]))
         {
-            blockVisible = false;
-            earthVisible = true;
-            roofVisible = false;
+            menuPic[6].visible=true;
+            menuPic[7].visible=true;
+            menuPic[8].visible=true;
+            menuPic[9].visible=true;
         }
 
 
-        if(blockVisible)
-        {
-            for(int npic=0; npic <= 2; npic++)
+         for(int npic=0; npic <= 9; npic++)
             {
-            drawPicture(menuPic[npic]);
+             txDeleteDC(menuPic[npic].image);
             }
-        }
-
-
-        if(earthVisible)
-        {
-            for(int npic=3; npic <= 5; npic++)
-            {
-             drawPicture(menuPic[npic]);
-            }
-        }
-
-        if(roofVisible)
-        {
-            for(int npic=6; npic <= 9; npic++)
-            {
-             drawPicture(menuPic[npic]);
-            }
-        }
-
 
 
         txSleep(50);
