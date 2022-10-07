199bb1fd830dea0785c78bd3173ffd8caa6ff031
KiriLL228x <100783309+KiriLL228x@users.noreply.github.com>
2022-10-07 19:33:45 +0400

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ struct Pictures
     int w;
     int h;
     bool visible;
+    string category;
 };
 //структура кнопки
 struct Button
@@ -15,8 +16,10 @@ struct Button
   int x;
   int y;
   const char* text;
+  string category;
 };
 //функция рисования кнопки
+
 void drawButton(Button btn)
 {
     //x=30 y=30
@@ -56,23 +59,29 @@ int main()
 
     HDC Fon3 = txLoadImage("img/fon3.bmp");
 
-    //Массив
-        Button btn[10];
-        btn[0] = {30,30,"Части стен"};
-        btn[1] = {200,30,"Крыша"};
-        btn[2] = {370,30,"Земли"};
+    int COUNT_BTN = 5;
+    int COUNT_MENU_PIC = 10;
 
-        Pictures menuPic[10];
-        menuPic[0] = {1000, 400, txLoadImage("img/Imgmenu/Wall1.bmp"), 90,90, false};
-        menuPic[1] = {1000, 300, txLoadImage("img/Imgmenu/Wall2.bmp"), 90,90, false};
-        menuPic[2] = {1000, 200, txLoadImage("img/Imgmenu/Wall3.bmp"), 90,90, false};
-        menuPic[3] = {1000, 400, txLoadImage("img/Imgmenu/Ground_10.bmp"), 90,90, false};
-        menuPic[4] = {1000, 300, txLoadImage("img/Imgmenu/Ground_11.bmp"), 90,90, false};
-        menuPic[5] = {1000, 200, txLoadImage("img/Imgmenu/Ground_12.bmp"), 90,90, false};
-        menuPic[6] = {1000, 400, txLoadImage("img/Imgmenu/Roof_A_04.bmp"), 90,40, false};
-        menuPic[7] = {1000, 300, txLoadImage("img/Imgmenu/Roof_A_05.bmp"), 90,40, false};
-        menuPic[8] = {1000, 200, txLoadImage("img/Imgmenu/Roof_B_04.bmp"), 90,40, false};
-        menuPic[9] = {1000, 100, txLoadImage("img/Imgmenu/Roof_B_05.bmp"), 90,40, false};
+    //Массив
+        Button btn[COUNT_BTN];
+        btn[0] = {30,30,"Части стен","Стены"};
+        btn[1] = {200,30,"Крыша","Крыша"};
+        btn[2] = {370,30,"Земля","Земля"};
+        btn[3] = {540,30,"Окна","Окна"};
+        btn[4] = {710,30,"Двери","Двери"};
+
+
+        Pictures menuPic[COUNT_MENU_PIC];
+        menuPic[0] = {1000, 400, txLoadImage("img/Imgmenu/Wall1.bmp"), 90,90, false,"Стены"};
+        menuPic[1] = {1000, 300, txLoadImage("img/Imgmenu/Wall2.bmp"), 90,90, false,"Стены"};
+        menuPic[2] = {1000, 200, txLoadImage("img/Imgmenu/Wall3.bmp"), 90,90, false,"Стены"};
+        menuPic[3] = {1000, 400, txLoadImage("img/Imgmenu/Ground_10.bmp"), 90,90, false,"Земля"};
+        menuPic[4] = {1000, 300, txLoadImage("img/Imgmenu/Ground_11.bmp"), 90,90, false,"Земля"};
+        menuPic[5] = {1000, 200, txLoadImage("img/Imgmenu/Ground_12.bmp"), 90,90, false,"Земля"};
+        menuPic[6] = {1000, 400, txLoadImage("img/Imgmenu/Roof_A_04.bmp"), 90,40, false,"Крыша"};
+        menuPic[7] = {1000, 300, txLoadImage("img/Imgmenu/Roof_A_05.bmp"), 90,40, false,"Крыша"};
+        menuPic[8] = {1000, 200, txLoadImage("img/Imgmenu/Roof_B_04.bmp"), 90,40, false,"Крыша"};
+        menuPic[9] = {1000, 100, txLoadImage("img/Imgmenu/Roof_B_05.bmp"), 90,40, false,"Крыша"};
 
 
     while(!GetAsyncKeyState(VK_ESCAPE))
@@ -83,36 +92,35 @@ int main()
         txBitBlt(txDC(), 0, 0, 1200, 900, Fon3);
 
         //Рисование кнопок
-        for(int nk=0; nk<3; nk++)
+        for(int nk=0; nk<5; nk++)
         {
             drawButton(btn[nk]);
         }
-//видимость картинок меню
-        if(click(btn[0]))
+
+        for(int npic=0; npic < COUNT_MENU_PIC; npic++)
         {
-            menuPic[0].visible=true;
-            menuPic[1].visible=true;
-            menuPic[2].visible=true;
-        }
-        if(click(btn[1]))
-        {
-            menuPic[3].visible=true;
-            menuPic[4].visible=true;
-            menuPic[5].visible=true;
-        }
-        if(click(btn[2]))
-        {
-            menuPic[6].visible=true;
-            menuPic[7].visible=true;
-            menuPic[8].visible=true;
-            menuPic[9].visible=true;
+            drawPicture(menuPic[npic]);
         }
 
-
-         for(int npic=0; npic <= 9; npic++)
+        //видимость картинок меню по  категории
+        for(int nk=0; nk<5; nk++)
+        {
+            if(click(btn[nk]))
             {
-             txDeleteDC(menuPic[npic].image);
+                for(int npic=0; npic < COUNT_MENU_PIC; npic++)
+                {
+                    menuPic[npic].visible= false;
+                    if(menuPic[npic].category == btn[nk].category)
+                    {
+                        menuPic[npic].visible=true;
+                    }
+                }
             }
+        }
+
+
+
+
 
 
         txSleep(50);
@@ -120,9 +128,9 @@ int main()
 
     }
 
-                for(int npic=0; npic <= 9; npic++)
+            for(int npic=0; npic < COUNT_MENU_PIC; npic++)
             {
-             txDeleteDC(menuPic[npic].image);
+                txDeleteDC(menuPic[npic].image);
             }
 
 
